697b40f8479cecb4dd9480348cc9959f82dff8fb
Ukasquared <ukachukwujudith95@gmail.com>
2023-07-28 06:33:51 +0000

    draws a straight line on terminal

---
+++ 0x04-low_level_programming/0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_line - draws straight line in the terminal
+*
+* Return: 0 (success)
+*/
+
+void print_line(int n)
+{
+	while (n--)
+		_putchar('_');
+	_putchar('\n');
+}
