f5f80c6d4a6001aefec4dbd0adde979686ea72c0
Ukasquared <ukachukwujudith95@gmail.com>
2023-07-28 06:27:11 +0000

    updated header file

--- 0x04-low_level_programming/0x04-more_functions_nested_loops/main.h
+++ 0x04-low_level_programming/0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,10 @@
 #include <stdio.h>
 int _isupper(int);
 int _isdigit(int);
-
+int mul(int,int);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
 #endif
